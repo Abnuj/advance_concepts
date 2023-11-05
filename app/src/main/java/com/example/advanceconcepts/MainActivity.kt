d14262b45eaf691ebf2be5527de28586bc5d6439
Abnuj <abnuj3224@gmail.com>
2023-11-05 11:08:27 +0530

    1st ver

---
+++ app/src/main/java/com/example/advanceconcepts/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.advanceconcepts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.advanceconcepts.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
